15755d4abc109886fbfc16042af1b0b0be2dd193
Dalton Smith <daltonsmith45@hotmail.com>
2024-09-12 17:29:15 -0500

    Finish content layout with placeholders

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,61 @@
+const aboutMeInterval = setInterval(() => {
+    if (document.getElementById("tab-1").checked) {
+        document.getElementById("tab-2").checked = true;
+    } else if (document.getElementById("tab-2").checked) {
+        document.getElementById("tab-3").checked = true;
+    } else if (document.getElementById("tab-3").checked) {
+        document.getElementById("tab-4").checked = true;
+    } else if (document.getElementById("tab-4").checked) {
+        document.getElementById("tab-5").checked = true;
+    } else {
+        document.getElementById("tab-1").checked = true;
+    } 
+}, 5000);
+
 function showHome() {
     document.getElementById("home-container").style.display = "block";
     document.getElementById("experience-container").style.display = "none";
     document.getElementById("about-container").style.display = "none";
+
+    document.getElementById("bmenub").checked = false;
 }
 
 function showExperience() {
     document.getElementById("home-container").style.display = "none";
     document.getElementById("experience-container").style.display = "block";
     document.getElementById("about-container").style.display = "none";
+
+    document.getElementById("bmenub").checked = false;
 }
 
 function showAbout() {
     document.getElementById("home-container").style.display = "none";
     document.getElementById("experience-container").style.display = "none";
     document.getElementById("about-container").style.display = "block";
+
+    document.getElementById("bmenub").checked = false;
+}
+
+function changeExperienceRight() {
+    if (document.getElementById("tabC-1").checked) {
+        document.getElementById("tabC-2").checked = true;
+    } else if (document.getElementById("tabC-2").checked) {
+        document.getElementById("tabC-3").checked = true;
+    } else {
+        document.getElementById("tabC-1").checked = true;
+    }
+}
+
+function changeExperienceLeft() {
+    if (document.getElementById("tabC-1").checked) {
+        document.getElementById("tabC-3").checked = true;
+    } else if (document.getElementById("tabC-2").checked) {
+        document.getElementById("tabC-1").checked = true;
+    } else {
+        document.getElementById("tabC-2").checked = true;
+    }
+}
+
+function cancelAboutMeGallery() {
+    clearInterval(aboutMeInterval);
 }
